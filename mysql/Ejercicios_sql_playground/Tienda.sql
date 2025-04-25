199315a040d174685fbd24a6c73b6301fbe8a223
JaimeBonBol <jaimebb2002@gmail.com>
2025-04-25 12:59:29 +0200

    Continuación ejercicios sql playground

--- mysql/Ejercicios_sql_playground/Tienda.sql
+++ mysql/Ejercicios_sql_playground/Tienda.sql
@@ -1,3 +1,154 @@
+-- CONSULTAS SENCILLAS
+
+-- 1.Lista el nombre de todos los productos que hay en la tabla producto.
+
+SELECT nombre 
+FROM producto;
+
+-- 2.Lista los nombres y los precios de todos los productos de la tabla producto.
+
+SELECT nombre, precio
+FROM producto;
+
+-- 3.Lista todas las columnas de la tabla producto.
+
+SELECT * 
+FROM producto;
+
+-- 4.Lista el nombre de los productos, el precio en euros y el precio en dólares estadounidenses (USD).
+
+SELECT nombre,precio AS precio_euros, (precio * 1.15) AS precio_dólares
+FROM producto;
+
+-- 5.Lista el nombre de los productos, el precio en euros y el precio en dólares estadounidenses (USD). Utiliza los siguientes alias para las columnas: nombre de producto, euros, dólares.
+
+SELECT nombre AS nombre_de_producto, precio AS euros, (precio * 1.15) AS dólares
+FROM producto;
+
+-- 6.Lista los nombres y los precios de todos los productos de la tabla producto, convirtiendo los nombres a mayúscula.
+
+SELECT UPPER(nombre), precio
+FROM producto;
+
+-- 7.Lista los nombres y los precios de todos los productos de la tabla producto, convirtiendo los nombres a minúscula.
+
+SELECT LOWER(nombre), precio
+FROM producto;
+
+-- 8.Lista el nombre de todos los fabricantes en una columna, y en otra columna obtenga en mayúsculas los dos primeros caracteres del nombre del fabricante.
+
+SELECT nombre, UPPER(SUBSTRING(nombre, 1, 2))
+FROM fabricante;
+
+SELECT nombre, UPPER(LEFT(nombre, 2))
+FROM fabricante;
+
+-- 9.Lista los nombres y los precios de todos los productos de la tabla producto, redondeando el valor del precio.
+
+SELECT nombre, ROUND(precio)
+FROM producto;
+
+
+-- 10.Lista los nombres y los precios de todos los productos de la tabla producto, truncando el valor del precio para mostrarlo sin ninguna cifra decimal.
+
+SELECT nombre, TRUNCATE(precio,0)
+FROM producto;
+
+-- 11.Lista el código de los fabricantes que tienen productos en la tabla producto.
+
+SELECT id_fabricante 
+FROM producto;
+
+-- 12.Lista el código de los fabricantes que tienen productos en la tabla producto, eliminando los códigos que aparecen repetidos.
+
+SELECT DISTINCT(id_fabricante)
+FROM producto;
+
+-- 13.Lista los nombres de los fabricantes ordenados de forma ascendente.
+
+SELECT nombre
+FROM fabricante
+ORDER BY nombre ASC;
+
+-- 14.Lista los nombres de los fabricantes ordenados de forma descendente.
+
+SELECT nombre 
+FROM fabricante
+ORDER BY nombre DESC;
+
+-- 15.Lista los nombres de los productos ordenados en primer lugar por el nombre de forma ascendente y en segundo lugar por el precio de forma descendente.
+
+SELECT nombre, precio
+FROM producto
+ORDER BY nombre ASC, precio DESC;
+
+-- 16.Devuelve una lista con las 5 primeras filas de la tabla fabricante.
+
+SELECT * 
+FROM fabricante 
+LIMIT 5;
+
+-- 17.Devuelve una lista con 2 filas a partir de la cuarta fila de la tabla fabricante. La cuarta fila también se debe incluir en la respuesta.
+
+-- Limita y muestra dos columnas a parte de la 3, el OFFSET es a partir de donde limita.
+SELECT * 
+FROM fabricante
+LIMIT 2 OFFSET 3;
+
+-- 18.Lista el nombre y el precio del producto más barato. (Utilice solamente las cláusulas ORDER BY y LIMIT)
+
+SELECT nombre, precio
+FROM producto
+ORDER BY precio ASC
+LIMIT 1;
+
+SELECT nombre, MIN(precio)
+FROM producto
+GROUP BY nombre
+ORDER BY MIN(precio) ASC
+LIMIT 1;
+
+-- 19.Lista el nombre y el precio del producto más caro. (Utilice solamente las cláusulas ORDER BY y LIMIT)
+
+SELECT nombre, precio 
+FROM producto
+ORDER BY precio DESC
+LIMIT 1;
+
+-- 20.Lista el nombre de todos los productos del fabricante cuyo código de fabricante es igual a 2.
+
+SELECT nombre
+FROM producto
+WHERE id_fabricante = 2;
+
+-- 21.Lista el nombre de los productos que tienen un precio menor o igual a 120€.
+
+SELECT nombre
+FROM producto
+WHERE precio <= 120;
+
+-- 22.Lista el nombre de los productos que tienen un precio mayor o igual a 400€.
+
+SELECT nombre
+FROM producto
+WHERE precio >= 400;
+
+-- 23.Lista el nombre de los productos que no tienen un precio mayor o igual a 400€.
+
+SELECT nombre
+FROM producto
+WHERE NOT ( precio >= 400);
+
+-- 24.Lista todos los productos que tengan un precio entre 80€ y 300€. Sin utilizar el operador BETWEEN.
+
+SELECT * FROM producto
+WHERE precio >= 80 AND precio <= 300;
+
+
+
+
+-- SUBCONSULTAS
+
 -- Ejerciccio 2.
 
 -- SELECT MAX(precio) FROM producto WHERE id_fabricante = (SELECT id FROM fabricante WHERE nombre = 'Lenovo');
