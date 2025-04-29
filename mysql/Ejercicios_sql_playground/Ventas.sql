29c461277491e77de4688e5b4d124013544b9abc
JaimeBonBol <jaimebb2002@gmail.com>
2025-04-29 19:09:11 +0200

    Continuación ejercicios sql

--- mysql/Ejercicios_sql_playground/Ventas.sql
+++ mysql/Ejercicios_sql_playground/Ventas.sql
@@ -72,3 +72,116 @@ ORDER BY nombre ASC;
 SELECT DISTINCT(nombre)
 FROM comercial
 WHERE (nombre LIKE '%el' OR nombre LIKE '%o');
+
+
+
+-- COMPOSICIÓN INTERNA
+
+-- 1.Devuelve un listado con el identificador, nombre y los apellidos de todos los clientes que han realizado algún pedido. El listado debe estar ordenado alfabéticamente y se deben eliminar los elementos repetidos.
+
+SELECT DISTINCT c.id, c.apellido1, c.apellido2, c.nombre
+FROM cliente AS c
+JOIN pedido AS p
+ON c.id = p.id_cliente
+ORDER BY c.apellido1, c.apellido2;
+
+-- 2.Devuelve un listado que muestre todos los pedidos que ha realizado cada cliente. El resultado debe mostrar todos los datos de los pedidos y del cliente. El listado debe mostrar los datos de los clientes ordenados alfabéticamente.
+
+SELECT p.*, c.*
+FROM pedido AS p
+JOIN cliente AS c
+ON p.id_cliente = c.id
+ORDER BY c.nombre, c.apellido1, c.apellido2;
+
+-- 3.Devuelve un listado que muestre todos los pedidos en los que ha participado un comercial. El resultado debe mostrar todos los datos de los pedidos y de los comerciales. El listado debe mostrar los datos de los comerciales ordenados alfabéticamente.
+
+SELECT  c.*, p.*
+FROM pedido AS p
+JOIN comercial AS c
+ON p.id_comercial = c.id
+ORDER BY c.nombre, c.apellido1, c.apellido2;
+
+-- 4.Devuelve un listado que muestre todos los clientes, con todos los pedidos que han realizado y con los datos de los comerciales asociados a cada pedido.
+
+SELECT cl.*, p.*, co.*
+FROM cliente AS cl
+JOIN pedido AS p
+ON cl.id = p.id_cliente
+JOIN comercial AS co 
+ON p.id_comercial = co.id;
+
+-- 5.Devuelve un listado de todos los clientes que realizaron un pedido durante el año 2017, cuya cantidad esté entre 300 € y 1000 €.
+
+SELECT c.*, p.*
+FROM cliente AS c
+JOIN pedido AS p
+ON c.id = p.id_cliente
+WHERE fecha BETWEEN '2017-01-01' AND '2017-12-31'
+AND total BETWEEN 300 AND 1000;
+
+-- 6.Devuelve el nombre y los apellidos de todos los comerciales que ha participado en algún pedido realizado por María Santana Moreno.
+
+SELECT DISTINCT co.apellido1, co.apellido2, co.nombre
+FROM comercial AS co
+JOIN pedido AS p
+ON co.id =p.id_comercial
+JOIN cliente AS cl
+ON p.id_cliente = cl.id
+WHERE cl.nombre LIKE 'María' AND cl.apellido1 LIKE 'Santana' AND cl.apellido2 LIKE 'Moreno';
+
+-- 7.Devuelve el nombre de todos los clientes que han realizado algún pedido con el comercial Daniel Sáez Vega.
+
+SELECT DISTINCT cl.apellido1, cl.apellido2, cl.nombre 
+FROM cliente AS cl
+JOIN pedido AS p 
+ON p.id_cliente = cl.id
+JOIN comercial AS co
+ON p.id_comercial = co.id
+WHERE co.nombre LIKE 'Daniel' AND co.apellido1 LIKE 'Sáez' AND co.apellido2 LIKE 'Vega';
+
+
+
+-- COMPOSICIÓN EXTERNA
+
+-- 1.Devuelve un listado con todos los clientes junto con los datos de los pedidos que han realizado. Este listado también debe incluir los clientes que no han realizado ningún pedido. El listado debe estar ordenado alfabéticamente por el primer apellido, segundo apellido y nombre de los clientes.
+
+-- En SQL, la tabla que aparece primero después de FROM es la tabla de la izquierda, y la que aparece después de LEFT JOIN es la de la derecha.
+
+
+SELECT cl.apellido1, cl.apellido2, cl.nombre, p.*
+FROM  cliente AS cl
+LEFT JOIN pedido AS p ON cl.id = p.id_cliente
+ORDER BY cl.apellido1, cl.apellido2, cl.nombre;
+
+
+-- 2.Devuelve un listado con todos los comerciales junto con los datos de los pedidos que han realizado. Este listado también debe incluir los comerciales que no han realizado ningún pedido. El listado debe estar ordenado alfabéticamente por el primer apellido, segundo apellido y nombre de los comerciales.
+
+SELECT co.*, p.*
+FROM comercial AS co
+LEFT JOIN pedido AS p
+ON co.id =p.id_comercial
+ORDER BY co.apellido1, co.apellido2, co.nombre;
+
+-- 3.Devuelve un listado que solamente muestre los clientes que no han realizado ningún pedido.
+
+SELECT cl.*, p.*
+FROM cliente AS cl
+LEFT JOIN pedido AS p 
+ON cl.id = p.id_cliente
+WHERE p.id_cliente IS NULL;
+
+-- 4.Devuelve un listado que solamente muestre los comerciales que no han realizado ningún pedido.
+
+SELECT co.*, p.*
+FROM comercial AS co
+LEFT JOIN pedido AS p
+ON co.id = p.id_comercial
+WHERE p.id_comercial IS NULL;
+
+-- 5.Devuelve un listado con los clientes que no han realizado ningún pedido y de los comerciales que no han participado en ningún pedido. Ordene el listado alfabéticamente por los apellidos y el nombre. En en listado deberá diferenciar de algún modo los clientes y los comerciales.
+
+
+
+
+-- CONSULTAS RESUMEN.
+
